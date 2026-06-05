7ea024369d4607300b2264c1c34e0415bada2ea0
kaushalkumar02 <kaushalthakur36035@gmail.com>
2026-06-05 12:07:53 +0530

    added playersscore program

---
+++ classwork/list _5th_june/playersscore.py
@@ -0,0 +1,11 @@
+players = []
+
+for i in range(11):
+    score = int(input(f"Player {i+1} Score: "))
+    players.append(("Player " + str(i+1), score))
+
+print("\n----- SCORECARD -----")
+print("Player\t\tScore")
+
+for i in range(11):
+    print(players[i][0], "\t\t", players[i][1])
